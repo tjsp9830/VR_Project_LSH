cc637bd6e8cbb0c877e1e8fa0779c342f3646209
tjsp9830 <mcgr1998@gmail.com>
2024-10-21 16:13:33 +0900

    [Chore] 파트2 수정중
    파트2 수정중 (세세한 벽 위치 조정, 각 Door별로 다른 스크립트 적용 등)

--- Assets/LSH/Scripts/DoorOpenFake.cs
+++ Assets/LSH/Scripts/DoorOpenFake.cs
@@ -2,30 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorOpen1 : MonoBehaviour
+public class DoorOpenFake : MonoBehaviour
 {
 
-    [SerializeField] Animator DoorUnlock;
-    [SerializeField] bool isDoor1Open;
+    [SerializeField] Animator FakeDoorUnlock;
+    [SerializeField] bool isDoorOpenFake;
 
     [SerializeField] CupSocket cupSocket;
 
     private void Awake()
     {
-        DoorUnlock = GetComponent<Animator>();
+        FakeDoorUnlock = GetComponent<Animator>();
     }
 
     private void Start()
     {
         cupSocket.doorUnLock += DoorUnlockSound;
-        isDoor1Open = false;
+        isDoorOpenFake = false;
     }
 
     public void DoorUnlockSound()
     {
-        if (isDoor1Open == false)
+        if (isDoorOpenFake == false)
         {
-            isDoor1Open = true;
+            isDoorOpenFake = true;
 
             // 소리재생 (잠금해제)
             Debug.Log("문1 잠금해제 사운드");
@@ -38,9 +38,9 @@ public class DoorOpen1 : MonoBehaviour
     public void DoorUnlockOpen()
     {
 
-        if (isDoor1Open == false)
+        if (isDoorOpenFake == false)
             return;
-        else 
+        else
         {
             //심플 그랩으로 문손잡이 잡았다 떼면
             Debug.Log("손잡이를 당겼다 놓자, \n문이 팍 하고 열렸다.");
@@ -49,14 +49,12 @@ public class DoorOpen1 : MonoBehaviour
             Debug.Log("문1 팍 열리는 사운드");
 
             // 문 열리는 애니메이션 재생
-            DoorUnlock.SetTrigger("UnLock1"); 
+            FakeDoorUnlock.SetTrigger("UnLockFake");
 
         }
 
 
 
     }
-
-
 
 }
