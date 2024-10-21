540870a3988a881352949d8d6366d693b1552c9f
tjsp9830 <mcgr1998@gmail.com>
2024-10-21 12:05:21 +0900

    [Feat] 퍼즐1 파훼시 잠긴 문 해금
    퍼즐과 문 오브젝트를 연동했다.
    (첫번째 퍼즐을 풀어야지만 첫번째 잠긴 문이 열리도록 구현)

---
+++ Assets/LSH/Scripts/CupSocket.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class CupSocket : MonoBehaviour
+{
+
+    [SerializeField] GameObject CupArea;
+    [SerializeField] GameObject CupPos;
+
+    public UnityAction doorUnLock;
+
+    
+    public void CupPosIsSocketPos(SelectEnterEventArgs Args)
+    {
+
+        // 꽂힌 인터렉터블 이벤트에 들어온 애가 내가 원하는 애랑 같은지를 판단해야함 
+        if (Args.interactableObject.transform == CupPos.transform) 
+        {
+            Debug.Log("트롤리 위 컵 갯수가 \n균형을 이뤘다. 문1 잠금 해제");
+            doorUnLock?.Invoke();
+
+        }
+
+
+    }
+
+}
---
+++ Assets/LSH/Scripts/DoorOpen1.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpen1 : MonoBehaviour
+{
+
+    [SerializeField] Animator DoorUnlock;
+    [SerializeField] bool isDoor1Open;
+
+    [SerializeField] CupSocket cupSocket;
+
+    private void Awake()
+    {
+        DoorUnlock = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        cupSocket.doorUnLock += DoorUnlockSound;
+        isDoor1Open = false;
+    }
+
+    public void DoorUnlockSound()
+    {
+        if (isDoor1Open == false)
+        {
+            isDoor1Open = true;
+
+            // 소리재생 (잠금해제)
+            Debug.Log("문1 잠금해제 사운드");
+
+        }
+
+
+    }
+
+    public void DoorUnlockOpen()
+    {
+
+        if (isDoor1Open == false)
+            return;
+        else 
+        {
+            //심플 그랩으로 문손잡이 잡았다 떼면
+            Debug.Log("손잡이를 당겼다 놓자, \n문이 팍 하고 열렸다.");
+
+            // 소리재생 (열리는 소리)
+            Debug.Log("문1 팍 열리는 사운드");
+
+            // 문 열리는 애니메이션 재생
+            DoorUnlock.SetTrigger("UnLock"); 
+
+        }
+
+
+
+    }
+
+
+
+}
